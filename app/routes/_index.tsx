30302ab4c42dd592feb469717f25a916923a988d
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-11 05:36:54 +0100

    add shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,14 @@
+import { Button } from "../components/ui/button";
 import type { MetaFunction } from "@remix-run/node";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card"
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +22,27 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
       <ul>
+        <Button >test</Button>
+        <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+      
+            </div>
+
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
         <li>
           <a
             target="_blank"
