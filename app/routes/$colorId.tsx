7232baa03236ae3432323437f5457561ec31cf4c
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-12 06:33:56 +0100

    add coolor name

--- app/routes/$colorId.tsx
+++ app/routes/$colorId.tsx
@@ -1,6 +1,10 @@
 import { Params, useParams } from "@remix-run/react";
 import React from "react";
 import { GeneratorNavigation } from "~/components/generator/options";
+import { colord, extend } from "colord";
+import namesPlugin from "colord/plugins/names";
+
+extend([namesPlugin]);
 
 type ColorParams = {
   colorId: string;
@@ -10,14 +14,28 @@ export default function colorId() {
   // Get the userId param from the URL.
   let { colorId } = useParams<ColorParams>();
 
+  console.log(colord("#664868").toName({ closest: true }));
+
   const colors: undefined | any = colorId && colorId.split("-");
 
+  const handleColorName = (colorHex: string) => {
+    console.log(colorHex, "colorHex");
+
+    let addHex: string = `#${colorHex}`;
+
+    console.log(addHex, "addHex");
+
+    console.log(colord(addHex).toName());
+    return colord(addHex).toName({ closest: true });
+  };
+
+  console.log(handleColorName("ff6347"), "test");
   return (
-    <div className="py-10 h-fit ">
+    <div className=" bg-blue-300 h-[100vh] ">
       <div className=" w-full flex justify-end">
         <GeneratorNavigation />
       </div>
-      {colors.map((color: any, index: number) => (
+      {colors.map((color: string, index: number) => (
         <div
           key={index}
           className="w-full h-36 flex px-[5px]"
@@ -28,7 +46,9 @@ export default function colorId() {
           <div className="self-end mb-1">
             <h3 className=" text-2xl text-white ">{color}</h3>
 
-            <p className=" text-sm text-white mt-[9px] ">{color}</p>
+            <p className=" text-sm text-white mt-[9px] ">
+              {handleColorName(color)}
+            </p>
           </div>
         </div>
       ))}
